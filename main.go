7eeb78b03b67d1b69ac760824ab71e9e9366e1e9
wesley601 <wesleypassos601@gmail.com>
2025-05-14 09:59:11 -0300

    feat: add port flag and print listen errors

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
-const PORT = ":5500"
+const DEFAULT_PORT = "5500"
 
 func main() {
+	p := flag.String("port", DEFAULT_PORT, "port to listen on")
+	flag.Parse()
+
+	if !isNumeric(*p) {
+		fmt.Println("Invalid port number")
+		return
+	}
+	port := ":" + *p
+
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	fmt.Printf("Start to serve at http://localhost%s\n", PORT)
-	http.ListenAndServe(PORT, nil)
+	fmt.Printf("Starting to serve at http://localhost%s\n", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
 }
